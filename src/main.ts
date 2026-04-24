19583b2d51ca8ac9b330f16c09963b99eefce79b
Lina Maria Caicedo <linamacaicedo@gmail.com>
2026-04-24 18:56:01 -0500

    fixing bugs vercel

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.ts'
 
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <section id="center">
   <div class="hero">
--- src/server.js
+++ src/server.js
@@ -24,6 +24,10 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+  });
+}
+
+export default app;
